e79e887810ed3b1f5120710f6d0d59b9bf790b05
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-01-16 16:27:38 +0100

    Séparez la logique de l'application de la présentation avec un gabarit Django

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,15 +4,8 @@ from listings.models import Band
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    return render(request, 'listings/hello.html',
+        {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
